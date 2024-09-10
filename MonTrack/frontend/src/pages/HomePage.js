7e436cb3fb6fb7dc5a4fede06e01cd4d56f8dd84
Jatin Varma <147076980+jatinweb2004@users.noreply.github.com>
2024-09-10 22:18:24 +0530

    Update HomePage.js

--- MonTrack/frontend/src/pages/HomePage.js
+++ MonTrack/frontend/src/pages/HomePage.js
@@ -55,6 +55,30 @@ const HomePage = () => {
     },
   ];
 
+
+
+// Pagination route
+app.get('/items', (req, res) => {
+  const page = parseInt(req.query.page) || 1;
+  const limit = parseInt(req.query.limit) || 5;
+
+  // Calculate the start and end index
+  const startIndex = (page - 1) * limit;
+  const endIndex = page * limit;
+  const result = data.slice(startIndex, endIndex); // Paginate the data
+  res.json({
+    page,
+    limit,
+    totalItems: data.length,
+    totalPages: Math.ceil(data.length / limit),
+    data: result,
+  });
+});
+
+
+
+
+
   useEffect(() => {
     const getAllTransactions = async () => {
       try {
